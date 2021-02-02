74a83d5e93abfb73889af11bdfb84e01955ed4df
Dmitry Averin <avedmigen@gmail.com>
2021-02-02 19:50:51 +0300

    Выполняет задание 3. Начинаем программировать

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import MainScreen from "../main-screen/main-screen";
+import PropTypes from "prop-types";
 
-const App = () => {
+const App = (props) => {
+  const {promo, movies} = props;
   return (
-    <MainScreen />
+    <>
+      <MainScreen
+        promo = {promo}
+        movies = {movies} />
+    </>
   );
+};
+
+App.propTypes = {
+  movies: PropTypes.array,
+  promo: PropTypes.object,
 };
 
 export default App;
---
+++ src/components/small-movie-card/small-movie-card.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const SmallMovieCard = (props) => {
+  const {movie} = props;
+
+  return (
+    <article className="small-movie-card catalog__movies-card">
+      <div className="small-movie-card__image">
+        <img src={movie.previewImage}
+          alt={movie.name} width="280" height="175"/>
+      </div>
+      <h3 className="small-movie-card__title">
+        <a className="small-movie-card__link" href="movie-page.html">{movie.name}</a>
+      </h3>
+    </article>
+  );
+};
+
+SmallMovieCard.propTypes = {
+  movie: PropTypes.object,
+};
+
+export default SmallMovieCard;
