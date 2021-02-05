b4338078871bb41ba76c7164920d3eb9fcc29e87
Dmitry Averin <avedmigen@gmail.com>
2021-02-05 11:43:11 +0300

    Добавляет маршрутизацию

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,45 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 
 import MainScreen from "../main-screen/main-screen";
+import SignInScreen from "../sign-in-screen/sign-in-screen";
+import MyListScreen from "../mylist-screen/mylist-screen";
+import MoviePageScreen from "../movie-page-screen/movie-page-screen";
+import AddReviewScreen from "../add-review-screen/add-review-screen";
+import PlayerScreen from "../player-screen/player-screen";
+import NotFoundScreen from "../not-found-screen/not-found-screen";
+
 import {movieShape} from "../../types";
 
 const App = ({promo, movies}) => (
-  <MainScreen
-    promo = {promo}
-    movies = {movies} />
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <MainScreen
+          promo = {promo}
+          movies = {movies} />
+      </Route>
+      <Route exact path="/login">
+        <SignInScreen />
+      </Route>
+      <Route exact path="/mylist">
+        <MyListScreen />
+      </Route>
+      <Route exact path="/films/:id">
+        <MoviePageScreen />
+      </Route>
+      <Route exact path="/films/:id/review">
+        <AddReviewScreen />
+      </Route>
+      <Route exact path="/player/:id">
+        <PlayerScreen />
+      </Route>
+      <Route>
+        <NotFoundScreen />
+      </Route>
+    </Switch>
+  </BrowserRouter>
 );
 
 App.propTypes = {
