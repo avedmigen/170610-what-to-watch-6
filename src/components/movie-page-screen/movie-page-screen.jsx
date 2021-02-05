4d451089f26091ab7d682eaf1847149dea4edd93
Dmitry Averin <avedmigen@gmail.com>
2021-02-05 22:06:25 +0300

    Добавляет описания фильмов по ID

--- src/components/movie-page-screen/movie-page-screen.jsx
+++ src/components/movie-page-screen/movie-page-screen.jsx
@@ -8,19 +8,21 @@ import SmallMovieCard from "../small-movie-card/small-movie-card";
 import LogoHeader from "../logo-header/logo-header";
 import LogoFooter from "../logo-footer/logo-footer";
 import PlayButton from "../play-button/play-button";
+import {findObjInArrayById} from "../../utils";
 
 const MoviePageScreen = (props) => {
 
   const films = props.movies;
+  const id = parseInt(props.props.match.params.id, 10);
+  const movie = findObjInArrayById(films, id);
 
-  const id = 12;
-
+  const {name, posterImage, backgroundImage, description, rating, scoresCount, director, starring, genre, released} = movie;
   return (
     <>
       <section className="movie-card movie-card--full">
         <div className="movie-card__hero">
           <div className="movie-card__bg">
-            <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel"/>
+            <img src={backgroundImage} alt={name}/>
           </div>
 
           <h1 className="visually-hidden">WTW</h1>
@@ -37,10 +39,10 @@ const MoviePageScreen = (props) => {
 
           <div className="movie-card__wrap">
             <div className="movie-card__desc">
-              <h2 className="movie-card__title">The Grand Budapest Hotel</h2>
+              <h2 className="movie-card__title">{name}</h2>
               <p className="movie-card__meta">
-                <span className="movie-card__genre">Drama</span>
-                <span className="movie-card__year">2014</span>
+                <span className="movie-card__genre">{genre}</span>
+                <span className="movie-card__year">{released}</span>
               </p>
 
               <div className="movie-card__buttons">
@@ -60,7 +62,7 @@ const MoviePageScreen = (props) => {
         <div className="movie-card__wrap movie-card__translate-top">
           <div className="movie-card__info">
             <div className="movie-card__poster movie-card__poster--big">
-              <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218"
+              <img src={posterImage} alt={name} width="218"
                 height="327"/>
             </div>
 
@@ -80,26 +82,19 @@ const MoviePageScreen = (props) => {
               </nav>
 
               <div className="movie-rating">
-                <div className="movie-rating__score">8,9</div>
+                <div className="movie-rating__score">{rating}</div>
                 <p className="movie-rating__meta">
                   <span className="movie-rating__level">Very good</span>
-                  <span className="movie-rating__count">240 ratings</span>
+                  <span className="movie-rating__count">{scoresCount} ratings</span>
                 </p>
               </div>
 
               <div className="movie-card__text">
-                <p>In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge
-                  Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave&apos;s friend and protege.</p>
+                <p>{description}</p>
 
-                <p>Gustave prides himself on providing first-class service to the hotel&apos;s guests, including satisfying
-                  the sexual needs of the many elderly women who stay there. When one of Gustave&apos;s lovers dies
-                  mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her
-                  murder.</p>
+                <p className="movie-card__director"><strong>Director: {director}</strong></p>
 
-                <p className="movie-card__director"><strong>Director: Wes Andreson</strong></p>
-
-                <p className="movie-card__starring"><strong>Starring: Bill Murray, Edward Norton, Jude Law, Willem Dafoe
-                  and other</strong></p>
+                <p className="movie-card__starring"><strong>Starring: {starring.map((star) => `${star}`).join(`, `) } and other</strong></p>
               </div>
             </div>
           </div>
@@ -111,7 +106,7 @@ const MoviePageScreen = (props) => {
           <h2 className="catalog__title">More like this</h2>
 
           <div className="catalog__movies-list">
-            {films.map((movie, i) => <SmallMovieCard key={movie + i} movie={movie} />)}
+            {films.map((film, i) => <SmallMovieCard key={film + i} movie={film} />)}
           </div>
 
         </section>
---
+++ src/utils.js
@@ -0,0 +1,3 @@
+export const findObjInArrayById = (array, id) => {
+  return array.find((item) => item.id === id);
+};
